c0ca546b484471eb5f96c2aa609308503f880451
Almir Almiev <madarat@mail.ru>
2021-04-29 23:59:29 +0300

    maven plugins починил

--- src/main/java/tech/itpark/Main.java
+++ src/main/java/tech/itpark/Main.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        String secret = "secret";
+        String secret = "secret!!!";
         var encode = Hex.encode(secret.getBytes(StandardCharsets.UTF_8));
         Hex.decode(encode);
     }
